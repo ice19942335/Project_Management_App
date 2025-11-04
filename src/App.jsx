03b59fba22953da8074fe0209df025cdfecf7eca
Aleksejs Birula <ice19942335@gmail.com>
2025-11-04 15:29:12 +0200

    Can add new project? select it from side-bar list and display project details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import { useState } from "react";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
     const [projectsState, setProjectsState] = useState({
@@ -9,15 +10,33 @@ function App() {
         projects: [],
     });
     
+    function handleSelectProject(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: id,
+            }
+        });
+    }
+    
     function handleStartAddProject() {
         setProjectsState((prevState) => {
             return {
                 ...prevState,
                 selectedProjectId: null,
             }
-        })
+        });
     }
 
+    function handleCancelAddProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+            }
+        })
+    }
+    
     function handleAddProject(projectData) {
         setProjectsState(prevState => {
             const projectId = Math.random();
@@ -34,9 +53,11 @@ function App() {
         });
     }
     
-    let content;
+    const selectedProject = projectsState.projects.find(
+        project => project.id === projectsState.selectedProjectId);
+    let content = <SelectedProject project={selectedProject} />
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject onAdd={handleAddProject}/>;
+        content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
     } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
     }
@@ -46,6 +67,8 @@ function App() {
             <ProjectsSidebar
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
+                onSelectProject={handleSelectProject}
+                selectedProjectId={projectsState.selectedProjectId}
             />
             {content}
         </main>
